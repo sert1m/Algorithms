ad861c52a95f9e0e25077fce25e8abd8c00a906f
Serhii Timokhin <serhii.timokhin@dev.zodiac.tv>
2017-08-14 13:07:32 +0300

    Base initialization

---
+++ src/kdtrees/PointSET.java
@@ -0,0 +1,31 @@
+package kdtrees;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET implements PointStorage {
+    public PointSET() {
+        
+    }
+    public boolean isEmpty() {
+        return false;
+    }
+    public int size() {
+        return 0;
+    }
+    public void insert(Point2D p) {
+        
+    }
+    public boolean contains(Point2D p) {
+        return false;
+    }
+    public void draw() {
+        
+    }
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+ }
---
+++ src/kdtrees/PointStorage.java
@@ -0,0 +1,22 @@
+package kdtrees;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+/** Interface for unit testing */
+public interface PointStorage {
+    // is the set empty?
+    public boolean isEmpty();
+    // number of points in the set
+    public int size();
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p);
+    // does the set contain point p?
+    public boolean contains(Point2D p);
+    // draw all points to standard draw
+    public void draw();
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect);
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p);
+}
